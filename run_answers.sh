6e64219b26a1ee698ded6cd13fe6169232c1f17b
sw1029 <yunsw62@gmail.com>
2025-06-07 03:07:52 +0900

    todo

--- run_answers.sh
+++ run_answers.sh
@@ -19,9 +19,9 @@ PY
 
 compile_all
 
-run_sample src.answers.academic_calendar_answer "5월 3일 학사일정 알려줘"
-run_sample src.answers.meals_answer "5월 3일 2학생회관 중식 뭐야"
-run_sample src.answers.notices_answer "컴퓨터공학부 공지 알려줘"
+run_sample src.answers.academic_calendar_answer "5월 1일 학사일정 알려줘"
+run_sample src.answers.meals_answer "5월 1일 2학생회관 중식 뭐야"
+run_sample src.answers.notices_answer "수학과 공지 알려줘"
 run_sample src.answers.shuttle_bus_answer "셔틀버스 노선 알려줘"
 run_sample src.answers.shuttle_bus_answer "셔틀버스 변동 사항 보여줘"
-run_sample src.answers.graduation_req_answer "컴퓨터공학부 졸업 요건 알려줘"
+run_sample src.answers.graduation_req_answer "인공지능학과 졸업 요건 알려줘"
