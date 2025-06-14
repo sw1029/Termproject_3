1d5982191a705ad9e3410b3af7f44d36622fe772
sw1029 <yunsw62@gmail.com>
2025-06-14 15:08:59 +0900

    model name

--- src/utils/config.py
+++ src/utils/config.py
@@ -4,6 +4,7 @@ from pathlib import Path
 class Settings(BaseSettings):
     data_dir: Path = Path('data')
     model_dir: Path = Path('model')
+    model_name: str = "dnotitia/Llama-DNA-1.0-8B-Instruct"
     openai_api_key: str = 'YOUR_API_KEY'
 
 settings = Settings()
